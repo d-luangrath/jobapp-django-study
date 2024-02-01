08c82b62984c6488531246cf49797a0ea6470829
Darla Luangrath <darla.luangrath@gmail.com>
2024-01-31 18:29:41 -0800

    Add URLs and views,display all jobs on home page

--- app/views.py
+++ app/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect
+from django.urls import reverse
+
 
 job_title = [
     "First Job",
-    "Second Job"
+    "Second Job",
+    "Third Job"
 ]
 
 job_description = [
     "First job description",
     "Second job description",
+    "Third job description",
 ]
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h3>Hello World<h3>")
+# def hello(request):
+#     return HttpResponse("<h3>Hello World<h3>")
+
+def job_list(request):
+    list_of_jobs = "<ul>"
+    for j in job_title:
+        job_id = job_title.index(j)
+        detail_url = reverse('jobs_detail', args=(job_id,))
+        list_of_jobs += f"<li><a href='{detail_url}'>{j}</a></li>"
+    list_of_jobs += "</ul>"
+    return HttpResponse(list_of_jobs)
 
 def job_detail(request, id):
-    return_html = f"<h1>{job_title[int(id)]}</h1> <h3>{job_description[int(id)]}</h3>"
-    return HttpResponse(return_html)
+    print(type(id))
+
+    try:
+        if id == 0:
+            return redirect(reverse('jobs_home'))
+        return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
+        return HttpResponse(return_html)
+    except:
+        return HttpResponseNotFound("Not Found")
