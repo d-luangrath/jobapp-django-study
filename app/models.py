5c13a57a531be5eed46979cb0c55a81a216855cd
Darla Luangrath <darla.luangrath@gmail.com>
2024-02-08 17:07:52 -0800

    Create multiple models and relationships in db; creat form for front end user

--- app/models.py
+++ app/models.py
@@ -1,12 +1,35 @@
 from django.db import models
 from django.utils.text import slugify
 
+class Skills(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    designation = models.CharField(max_length=200)
+
+    def __repr__(self):
+        return f"Author({self.pk}, name={self.name}, company={self.company})"
+
+class Location(models.Model):
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+
 class JobPost(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+    expiry = models.DateField(null=True)
     salary = models.IntegerField()
     slug = models.SlugField(null=True, max_length=40, unique=True)
+    location = models.OneToOneField(Location, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    skills = models.ManyToManyField(Skills)
 
     def save(self, *args, **kwargs):
         if not self.id:
@@ -14,5 +37,8 @@ class JobPost(models.Model):
         return super(JobPost, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.title
+        return f"{self.title} with salary {self.salary}"
+    
+
+
         
