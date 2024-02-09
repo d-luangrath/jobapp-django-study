d4ba2ad3c3f78a56072d27d17ed13a07d0152232
Darla Luangrath <darla.luangrath@gmail.com>
2024-02-08 19:38:32 -0800

    Add serverside validations

--- subscribe/views.py
+++ subscribe/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def subscribe(request):
-    context={}
+    email_error_empty = ""
+    if request.POST:
+        email = request.POST['email']
+        print(f"\033[36m█▓▒░ {__name__} | POST REQUEST: {email}\033[0m")
+        if email == "":
+            email_error_empty = "No email entered."
+    context={"email_error_empty": email_error_empty}
     return render(request, 'subscribe/subscribe.html', context)
