66e4d03636f06bf1a17e18cb72b2f77fc4e59f73
Darla Luangrath <darla.luangrath@gmail.com>
2024-02-08 20:11:10 -0800

    Save data to the DB

--- subscribe/views.py
+++ subscribe/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from subscribe.models import Subscribe
 
-# Create your views here.
 def subscribe(request):
     email_error_empty = ""
     if request.POST:
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
         email = request.POST['email']
         print(f"\033[36m█▓▒░ {__name__} | POST REQUEST: {email}\033[0m")
         if email == "":
-            email_error_empty = "No email entered."
+            email_error_empty = "No email entered"
+
+        subscribe = Subscribe(
+                            first_name=first_name,
+                            last_name=last_name,
+                            email=email
+                        )    
+        subscribe.save()
     context={"email_error_empty": email_error_empty}
     return render(request, 'subscribe/subscribe.html', context)
