817e6a5e84c2ecaede700e2216c83401897b905f
Darla Luangrath <darla.luangrath@gmail.com>
2024-02-12 15:30:49 -0800

    Convert subscribe form into ModelForm

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -1,18 +1,23 @@
 from django import forms
+from subscribe.models import Subscribe
 
-def validate_comma(value):
-    if "," in value:
-        raise forms.ValidationError("Invalid Last Name")
-    return value
+class SubscribeForm(forms.ModelForm):
+    """Linking the model to a form and adding the fields wanted to render and display"""
+    class Meta:
+        model=Subscribe
+        fields='__all__'
 
-class SubscribeForm(forms.Form):
-    first_name = forms.CharField(max_length=100, required=False, label="Enter first name", help_text="Enter characters only")
-    last_name = forms.CharField(max_length=100, disabled=False, validators=[validate_comma])
-    email = forms.EmailField(max_length=100)
 
-    # def clean_first_name(self):
-    #     data = self.cleaned_data['first_name']
-    #     if "," in data:
-    #         raise forms.ValidationError("Invalid First Name")
-    #     return data
+
+# def validate_comma(value):
+#     if "," in value:
+#         raise forms.ValidationError("Invalid Last Name")
+#     return value
+
+# class SubscribeForm(forms.Form):
+#     first_name = forms.CharField(max_length=100, required=False, label="Enter first name", help_text="Enter characters only")
+#     last_name = forms.CharField(max_length=100, disabled=False, validators=[validate_comma])
+#     email = forms.EmailField(max_length=100)
+
+
     
