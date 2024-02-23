2cf813127a69f419a738c189825d9076b6ef7ddc
Darla Luangrath <darla.luangrath@gmail.com>
2024-02-22 20:16:24 -0800

    update db settings to use postgres

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -79,10 +79,18 @@ WSGI_APPLICATION = 'jobapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': '<database_name>',
+       'USER': '<database_username>',
+       'PASSWORD': '<password>',
+       'HOST': '<database_hostname_or_ip>',
+       'PORT': '<database_port>',
+   }
 }
 
 
