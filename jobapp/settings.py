5a38a9204b3b044d6e4bf71faf6b7ce69e46cfa9
Darla Luangrath <darla.luangrath@gmail.com>
2024-02-22 20:22:05 -0800

    update db settings

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -85,11 +85,11 @@ DATABASES = {
     # }
     'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': '<database_name>',
-       'USER': '<database_username>',
-       'PASSWORD': '<password>',
-       'HOST': '<database_hostname_or_ip>',
-       'PORT': '<database_port>',
+    #    'NAME': '<database_name>',
+    #    'USER': '<database_username>',
+    #    'PASSWORD': '<password>',
+    #    'HOST': '<database_hostname_or_ip>',
+    #    'PORT': '<database_port>',
    }
 }
 
